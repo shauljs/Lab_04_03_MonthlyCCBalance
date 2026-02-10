79b17d10f6116546b37379cb3a1cf4e009324921
shauljs <shauljs@mail.uc.edu>
2026-02-10 16:21:43 -0500

    Final Commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //Declarations
+        double startingBalance = 5000;
+        double oneMonthBalance;
+        double twoMonthBalance;
+
+        oneMonthBalance = (startingBalance * .17) + startingBalance;
+
+        System.out.println("Your balance due after one month is $" + oneMonthBalance + "0.");
+
+        twoMonthBalance = (oneMonthBalance * .17) + oneMonthBalance;
+
+        System.out.println("Your balance due after two motnhs is $" + twoMonthBalance + "0.");
+    }
+}
